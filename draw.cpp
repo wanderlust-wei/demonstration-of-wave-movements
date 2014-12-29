0d0870851c0d2339845986f7abb469c2730358f8
wanderlust <weiyihuan@baixing.com>
2014-12-30 00:42:15 +0800

    Create draw.cpp

---
+++ draw.cpp
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+#include<fstream.h>
+#include<math.h>
+#include<time.h>
+#include<graphics.h>
+#include<conio.h>
+
+
+/*******************************************
+* This function draws a pendulum given     *
+* the pendulum length and angle            *
+* Input: length and angle			       *
+********************************************/
+void DrawWave(float Amp1,float Amp2, float omega1,float omega2, float k1,float k2, float time) ;
+
+/*******************************************
+* This function draws pendulums according  *
+* to the place of mouse by dragging        *
+* Input: length						       *
+********************************************/
+//int DragAngle(float length);
